d96c340fc895643659a82b6b829e70f498fee0b7
Mzheldin <mzheldin@yandex.ru>
2019-05-27 14:28:22 +0300

    remove

--- java/com/geekbrains/repositories/CoursesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.geekbrains.repositories;
-
-import com.geekbrains.entities.Course;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CoursesRepository extends CrudRepository<Course, Long> {
-}
