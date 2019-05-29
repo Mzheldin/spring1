074f61ed7ddbd3ac9dd64620d91a9ddf8617b081
Mzheldin <mzheldin@yandex.ru>
2019-05-29 16:09:15 +0300

    del

--- java/com/geekbrains/geekspring/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.geekbrains.geekspring.repositories;
-
-import com.geekbrains.geekspring.entities.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findOneByUserName(String userName);
-}
